83fcf2574ffbdce7b713457085004c1e555f36e6
Gajjar <hqlq@chevron.com>
2020-08-17 15:56:17 +0800

    fork analytics plugin to get it to work with OutSystems

---
+++ types/index.d.ts
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+declare namespace AppCenter {
+    let Analytics: {
+        trackEvent(
+            eventName: string,
+            properties: {},
+            success: () => void,
+            error: (error: any) => void
+        ): void;
+
+        isEnabled(
+            success: (isEnabled: boolean) => void,
+            error: (error: any) => void
+        ): void;
+
+        setEnabled(
+            secret: string,
+            enabled: boolean,
+            success: () => void,
+            error: (error: any) => void
+        ): void;
+    };
+}
